674dc3f9649cf01238a185996dc8ae2f4cd2cd68
arthurbabey <arthur.babey@epfl.ch>
2019-11-16 13:39:57 +0100

    update sub and helpers

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import os,sys
 from PIL import Image
 from sklearn.preprocessing import PolynomialFeatures
-
+from skimage import feature
 
 # Helper functions
 
@@ -146,3 +146,37 @@ def poly_augmentation(X, degree):
     poly = PolynomialFeatures(degree, interaction_only=False, include_bias=True)
 
     return poly.fit_transform(X)
+
+# Convert array of labels to an image
+
+def label_to_img(imgwidth, imgheight, w, h, labels):
+    im = np.zeros([imgwidth, imgheight])
+    idx = 0
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            im[j:j+w, i:i+h] = labels[idx]
+            idx = idx + 1
+    return im
+
+def make_img_overlay(img, predicted_img):
+    w = img.shape[0]
+    h = img.shape[1]
+    color_mask = np.zeros((w, h, 3), dtype=np.uint8)
+    color_mask[:,:,0] = predicted_img*255
+
+    img8 = img_float_to_uint8(img)
+    background = Image.fromarray(img8, 'RGB').convert("RGBA")
+    overlay = Image.fromarray(color_mask, 'RGB').convert("RGBA")
+    new_img = Image.blend(background, overlay, 0.2)
+    return new_img
+
+"""
+
+def fanny_filter(im, sigma=2):
+
+
+    edges = feature.canny(im, sigma)
+
+    return edges
+
+"""
